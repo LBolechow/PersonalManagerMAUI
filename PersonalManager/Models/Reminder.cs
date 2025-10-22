9dbdf7dfdde02e5725d15b6523914028ca602d96
LBolechow <110845720+LBolechow@users.noreply.github.com>
2025-10-22 15:04:25 +0200

    Update Reminder.cs

--- PersonalManager/Models/Reminder.cs
+++ PersonalManager/Models/Reminder.cs
@@ -10,14 +10,14 @@ namespace PersonalManager.Models
 {
     public class Reminder
     {
-        public int Id { get; set; } // Unikalny identyfikator przypomnienia
-        public DateTime ReminderDate { get; set; } // Data i godzina przypomnienia
-        public string Frequency { get; set; } // Częstotliwość (np. codziennie, co tydzień)
+        public int Id { get; set; }
+        public DateTime ReminderDate { get; set; } 
+        public string Frequency { get; set; } 
 
-        public int? EventId { get; set; } // Klucz obcy wskazujący na wydarzenie
-        public Event Event { get; set; } // Obiekt wydarzenia (jeśli jest powiązane)
+        public int? EventId { get; set; } 
+        public Event Event { get; set; }
 
-        public int? MyTaskId { get; set; } // Klucz obcy wskazujący na zadanie
-        public MyTask MyTask { get; set; } // Obiekt zadania (jeśli jest powiązane)
+        public int? MyTaskId { get; set; } 
+        public MyTask MyTask { get; set; }
     }
 }
