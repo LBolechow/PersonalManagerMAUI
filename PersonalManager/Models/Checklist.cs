9c79400c9bf1f4f4a5a744cd4e1f07693084b85e
LBolechow <110845720+LBolechow@users.noreply.github.com>
2025-10-22 15:02:44 +0200

    Update Checklist.cs

--- PersonalManager/Models/Checklist.cs
+++ PersonalManager/Models/Checklist.cs
@@ -9,14 +9,14 @@ namespace PersonalManager.Models
 {
     public class Checklist
     {
-        public int Id { get; set; } // Unikalny identyfikator checklisty
-        public string Title { get; set; } // Tytuł checklisty (np. "Zakupy", "Rzeczy do zrobienia")
+        public int Id { get; set; } 
+        public string Title { get; set; } 
 
-        public int? EventId { get; set; } // Klucz obcy wskazujący na wydarzenie
-        public Event Event { get; set; } // Powiązane wydarzenie (jeśli istnieje)
+        public int? EventId { get; set; } 
+        public Event Event { get; set; } 
 
-        public int? MyTaskId { get; set; } // Klucz obcy wskazujący na zadanie
-        public MyTask MyTask { get; set; } // Powiązane zadanie (jeśli istnieje)
+        public int? MyTaskId { get; set; }
+        public MyTask MyTask { get; set; } 
 
     }
 }
